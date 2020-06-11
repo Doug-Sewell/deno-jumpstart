b08073ad95b8780ffd02623826843fed4c152c97
Doug Sewell <DouglasASewell@gmail.com>
2020-06-11 19:25:20 -0400

    Learned how to use 3rd part modules. Utilized denon for watching files and the case module.

--- servers.ts
+++ servers.ts
@@ -1,10 +1,25 @@
 //http module {server}
+/*
 import { serve } from "https://deno.land/std/http/server.ts";
 
 const server = serve({port: 3000});
 console.log('Listening for requests on port 3000');
 
 for await(const req of server) {
+    console.log('request made');
     const url= req.url;
-    req.respond({body: `Hello ninjas, you visited ${url}`});
+    req.respond({body: `Hello again ninjas, you visited ${url}`});
 }
+*/
+
+
+import { camelCase, paramCase, pascalCase, snakeCase } from "https://deno.land/x/case/mod.ts";
+
+const text = 'Hello again ninjas';
+
+const camel = camelCase(text);
+const param = paramCase(text);
+const pascal = pascalCase(text);
+const snake = snakeCase(text);
+
+console.log(camel,param,pascal,snake);
